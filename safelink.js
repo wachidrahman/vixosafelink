7cf35f7b2315fb6f721e567a42b6e72d6e63f875
wachidrahman <wachidrahman@gmail.com>
2018-11-24 17:23:58 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -19,12 +19,17 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*8)+ 1);
 var xxx=null;
 if(x=="1"){xxx="the-most-important-steps-to-successful-career-change.html"}
 if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
 if(x=="3"){xxx="how-to-purchase-used-automobile.html"}
-if(x=="4"){xxx="what-type-of-home-buyer-are-you.html"}a_to_ve[i].href="https://www.vixolite.com/2018/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="what-type-of-home-buyer-are-you.html"}
+if(x=="5"){xxx="top-10-best-educational-apps-for-android.html"}
+if(x=="6"){xxx="health-insurance-why-it-is-important.html"}
+if(x=="7"){xxx="things-to-know-before-considering-health-insurance.html"}
+if(x=="8"){xxx="getting-health-insurance-in-emergency.html"}
+a_to_ve[i].href="https://www.vixolite.com/2018/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
